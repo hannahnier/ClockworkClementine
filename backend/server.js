624ee7dada1f24003ccdc18ba7228d2e05e1f378
hannahnier <hannahr@posteo.de>
2024-07-13 10:46:53 +0200

    change URL addresses from localhost to real URLs

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,9 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+  cors({ origin: "http://clockworkclementine.onrender.com", credentials: true }) // http://localhost:5173
+);
 
 await connectToDb(DB_CONNECTION_STRING);
 
